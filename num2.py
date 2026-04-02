7ad609d0703e31a9e72e19d911e9a932b81f8d9e
Patel Nisarg Kalpeshbhai <2203031280012@paruluniversity.ac.in>
2026-04-03 00:23:34 +0530

    2nd April 2026

---
+++ num2.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+
+# 1. SHAPE & INSPECTION
+
+arr_1d = np.array([22, 25, 21, 18, 24, 27, 20, 19, 23, 26])
+
+print("1D Array Shape:", arr_1d.shape)  # Output: (10,)
+print("---")
+
+#RESHAPING DATA
+
+# Original array with 20 elements
+data = np.array([22, 25, 21, 18, 24, 27, 20, 19, 23, 26, 22, 25, 21, 18, 24, 27, 20, 19, 23, 26])
+
+# Manual Reshape
+grid_4x5 = data.reshape(4, 5)
+
+# Auto-Reshape
+data_12 = data[:12]
+grid_auto = data_12.reshape(3, -1) 
+
+print("Reshaped 4x5 Grid:\n", grid_4x5)
+print("\nReshaped 3x4 (Auto) Grid:\n", grid_auto)
+print("---")
+
+
+# SLICING & CREATION
+square = np.arange(0, 12).reshape(3, 4)[0:2, 0:2]
+
+print("Top-left 2x2 slice:\n", square)
+print("---")
+
+
+# BOOLEAN INDEXING
+
+scores = np.array([
+    [85, 90, 78, 92],
+    [88, 82, 95, 89],  
+    [75, 80, 85, 70]   
+])
+
+mask = scores > 90
+
+print("Boolean Mask (Scores > 90):\n", mask)
+print("\nActual High Scores:", scores[mask])
